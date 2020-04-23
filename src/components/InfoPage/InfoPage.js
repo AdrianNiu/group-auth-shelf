19c06f38f1a6d1e7c8e0063ccbad13c5fa5549eb
Adrian Niu <niuxx107@umn.edu>
2020-04-23 15:36:36 -0400

    add item component added

--- src/components/InfoPage/InfoPage.js
+++ src/components/InfoPage/InfoPage.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import InputForm from '../InputForm/InputForm';
 
 // This is one of our simplest components
 // It doesn't have local state, so it can be a function component.
@@ -10,6 +11,7 @@ const InfoPage = () => (
     <p>
       Shelf Page
     </p>
+    <InputForm />
   </div>
 );
 
